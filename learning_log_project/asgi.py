7cfe2445cf638c0d38c03421188983f7bf49417b
CthulhuIsRight <47577221+CthulhuIsRight@users.noreply.github.com>
2025-08-21 13:36:42 -0700

    Rename Django project to be more descriptive and update project structure

--- learning_log_project/asgi.py
+++ learning_log_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project_11 project.
+ASGI config for learning_log_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_11.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log_project.settings')
 
 application = get_asgi_application()
--- learning_log_project/urls.py
+++ learning_log_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project_11 project.
+URL configuration for learning_log_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
